ba002f2c797596b40cacf472b90ddb7a9b0db32a
iamnotsnani <zeynallisnani@gmail.com>
2025-04-30 13:47:48 +0400

    Added FlightService interface and implementation

--- src/main/java/az/edu/bhos/finalProject/service/FlightService.java
+++ src/main/java/az/edu/bhos/finalProject/service/FlightService.java
@@ -1,4 +1,19 @@
 package az.edu.bhos.finalProject.service;
 
+import az.edu.bhos.finalProject.entity.Flight;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.List;
+
 public interface FlightService {
+    List<Flight> getAllFlights();
+
+    Flight getFlightById(String flightID);
+
+    List<Flight> searchAvailableFlights(String departure, String destination, LocalDate date, int requestedSeats);
+
+    boolean bookFlight(String flightID, int requestedSeats) throws IOException;
+
+    boolean cancelBooking(String flightID, int seatsToCancel) throws IOException;
 }
