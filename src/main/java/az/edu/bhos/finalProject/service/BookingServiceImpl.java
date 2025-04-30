1fb9a365a521a9c9a97567214d66814e68899aa4
iamnotsnani <zeynallisnani@gmail.com>
2025-04-30 14:13:03 +0400

    Add BookingService interface and implementation

--- src/main/java/az/edu/bhos/finalProject/service/BookingServiceImpl.java
+++ src/main/java/az/edu/bhos/finalProject/service/BookingServiceImpl.java
@@ -1,4 +1,45 @@
 package az.edu.bhos.finalProject.service;
 
-public class BookingServiceImpl {
+import az.edu.bhos.finalProject.dao.BookingDAO;
+import az.edu.bhos.finalProject.entity.Booking;
+
+import java.io.IOException;
+import java.util.List;
+
+public class BookingServiceImpl implements BookingSerivce {
+    private final BookingDAO bookingDAO;
+
+    public BookingServiceImpl(BookingDAO bookingDAO) {
+        this.bookingDAO = bookingDAO;
+    }
+
+    @Override
+    public Booking getBookingById(String id) {
+        return bookingDAO.getById(id);
+    }
+
+    @Override
+    public List<Booking> getAllBookings() {
+        return bookingDAO.getAll();
+    }
+
+    @Override
+    public boolean createBooking(Booking booking) throws IOException {
+        return bookingDAO.insert(booking);
+    }
+
+    @Override
+    public boolean deleteBooking(Booking booking) throws IOException {
+        return bookingDAO.delete(booking);
+    }
+
+    @Override
+    public boolean deleteBookingById(String id) throws IOException {
+        return bookingDAO.deleteById(id);
+    }
+
+    @Override
+    public List<Booking> getBookingsByPassenger(String name, String surname) {
+        return bookingDAO.getByPassenger(name, surname);
+    }
 }
